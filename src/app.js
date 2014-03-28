101c3e8fba4b315945b3471464f4b12f6b7a7c25
John Woodruff <johnwoodruff91@gmail.com>
2014-03-27 23:55:48 -0600

    Added bootstrap, images, and a whole bunch of content and styling.

---
+++ src/app.js
@@ -0,0 +1,13 @@
+$(window).on('resize', function() {
+	var container = $('#topDetail');
+	var height = $(window).height();
+	var imgHeight = $("#heroSection").height();
+
+	if(imgHeight > height){
+		container.css({marginTop: (height-80)});
+	}
+	else{
+		container.css({marginTop: (imgHeight-80)});
+	}
+
+}).trigger('resize');
